267479dc128b9b2b32813b1eb804bfab98b00e91
CLAUDIU STEFAN <stefanclaudiu@programmer.net>
2024-02-26 20:59:37 +0000

    removing and re-adjusting directory structure

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,73 @@
-<script setup>
-
-</script>
-
 <template>
-  <div id="app">
-   
-   
+  <div id="app" v-cloak>
+    <Header :siteName="siteName" :cartCounter="1" @cart-on-click="toggleCart"></Header>
+    <div class="container-fluid p-3 bg-success">
+      <component :is="currentView"></component>
+    </div>
+    <CopyrightBar :year="copyright_year" :siteName="siteName"></CopyrightBar>
   </div>
 </template>
 
-<style scoped>
+<script>
+import LessonView from './views/LessonView.vue';
+import CheckoutView from './views/CheckoutView.vue';
+import Header from './components/Header.vue';
+import CopyrightBar from './components/CopyrightBar.vue';
 
-</style>
+export default {
+  name: "App",
+  data() {
+    return {
+      siteName: "After School Hub",
+      copyright_year: 2023,
+      cart: [],
+      lessons: [],
+      endpoints: {
+        host: "https://after-school-hub-env2.eba-iijwxnmm.eu-west-2.elasticbeanstalk.com",
+        lessons: "/lessons",
+        orders: "/orders",
+        images: "/images"
+      },
+      currentView: LessonView,
+    }
+  },
+  components: { LessonView, CheckoutView, Header, CopyrightBar },
+  methods:{
+    async getLessons() {
+      try {
+        const endpoint = `${this.endpoints.host}${this.endpoints.lessons}`;
+
+        const response = await fetch(endpoint);
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const lesson = await response.json();
+
+        return lesson;
+
+      } catch (error) {
+        console.error('There was a problem in fetching lesson spaces:', error);
+        throw error;
+      }
+    },
+    fetchLessons() {
+      this.getLessons().then((lessons) => {
+        this.lessons = lessons;
+      })
+    },
+    toggleCart(){
+      if(this.currentView === LessonView){
+        this.currentView = CheckoutView
+      }else{
+        this.currentView = LessonView
+      }
+    }
+  },
+  beforeMount() {
+    this.fetchLessons();
+    console.log(this.lessons);
+  },
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import "./assets/css/base.css"
+import "./assets/css/main.css"
+import "./assets/css/fonts.css"
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('../src/service-worker.js')
