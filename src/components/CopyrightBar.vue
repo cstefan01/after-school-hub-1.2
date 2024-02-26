ce8ebc2d3dc9ff8de6e96d39e764fa22734e83e5
CLAUDIU STEFAN <stefanclaudiu@programmer.net>
2024-02-26 20:58:11 +0000

    implementing copyright bar component

---
+++ src/components/CopyrightBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <!-- Start of Footer Copyright -->
+    <div class="copyright container-fluid">
+        <span>Copyright &copy; {{ year }}, {{ siteName}} | All Rights Reserved.</span>
+    </div>
+    <!-- End of Footer Copyright -->
+</template>
+
+<script>
+export default {
+    name: "CopyrightBar",
+    props:{
+        siteName: String,
+        year: Number
+    }
+}
+</script>
+
+<style scoped>
+/* ------------ Start of Footer ------------ */
+.copyright {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 70px;
+    color: #fff;
+    background: #BF4C41;
+}
+/* ------------ End of Footer ------------- */
+</style>
